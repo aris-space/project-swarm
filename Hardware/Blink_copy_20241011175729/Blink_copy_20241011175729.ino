9cb91a668e120f1d791354c1d28d7a7f84e315ed
ninoliner <nliner@student.ethz.ch>
2024-11-12 09:38:02 +0100

    test

--- Hardware/Blink_copy_20241011175729/Blink_copy_20241011175729.ino
+++ Hardware/Blink_copy_20241011175729/Blink_copy_20241011175729.ino
@@ -26,7 +26,7 @@
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  delay(2000);
+  delay(3000);
 }
 
 // the loop function runs over and over again forever
