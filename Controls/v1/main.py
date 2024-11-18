95445da7ab4099ecab97d0a508460719e47da5f2
ninoliner <nliner@student.ethz.ch>
2024-11-18 17:03:39 +0100

    /

--- Controls/v1/main.py
+++ Controls/v1/main.py
@@ -13,5 +13,4 @@ if __name__ == "__main__":
     pid_params = load_yaml('main/Controls/v1/sac_/config/pid_params.yaml')
     init_params = load_yaml('main/Controls/v1/01config/constants.yaml')['initial_orientation']
 
-
     llc = LLC(pid_params, init_params, llc_freq)
