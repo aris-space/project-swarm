4ff5b9bf9d37961c7beab7124bc41f426d162b92
ninoliner <nliner@student.ethz.ch>
2024-11-18 08:42:21 +0100

    /

--- Controls/Single Agent Controller/controllers/forward_ctrl.py
+++ Controls/Single Agent Controller/controllers/forward_ctrl.py
@@ -1,47 +1,50 @@
 from controllers.pid import PID
-class forward_ctrl:
+class fwd_ctrl:
     def __init__(self, pid_params, controller_freq):
-        self.forward_controller = PID(**pid_params['abs'])
-        self.forward_rate_controller = PID(**pid_params['rate'])
-        self.current_forward = 0
-        self.desired_forward = 0
-        self.current_forward_rate = 0
-        self.desired_forward_rate = 0
-        self.desired_thrust_z = 0
+        self.fwd_controller = PID(**pid_params['abs'])
+        self.fwd_rate_controller = PID(**pid_params['rate'])
+
+        #self.current_fwd = 0
+        self.desired_fwd = 0
+        #self.current_fwd_rate = 0
+        self.desired_fwd_rate = 0
+        #self.current_thrust = 0
+        self.desired_thrust = 0
+
+        self.current_detectable_fwd = 0
+        self.current_detectable_fwd_rate = 0
+        self.current_detectable_thrust = 0
+
         self.dt = 1/controller_freq
 
-        self.current_detectable_forward_state = {}
-        self.forwards = [] 
-        self.detectable_forwards = []
+    def update_dd(self, desired_fwd): #desired fwd, should be called when new command is given
+        self.desired_fwd = desired_fwd
+        return self.desired_fwd
 
-    def update_dd(self, desired_forward): #desired forward, should be called when new command is given
-        self.desired_forward = desired_forward
-        return self.desired_forward
+    def update_cdd(self, imu_fwd): #current fwd, should be called if new fwd data is available
+        self.current_detectable_fwd = imu_fwd
+        return self.current_detectable_fwd
 
-    def update_cd(self, current_forward): #current forward, should be called if new forward data is available
-        self.current_forward = current_forward
-        return self.current_forward
-
-    def update_ddr(self): #desired forward rate, should be called after new forward data is given
-        self.desired_forward_rate = self.forward_controller.update(self.desired_forward, self.current_forward, self.dt)
-        return self.desired_forward_rate
+    def update_ddr(self): #desired fwd rate, should be called after new fwd data is given
+        self.desired_fwd_rate = self.fwd_controller.update(self.desired_fwd, self.current_detectable_fwd, self.dt)
+        print("desired fwd rate: ", self.desired_fwd_rate)
+        return self.desired_fwd_rate
    
-    def update_cdr(self, current_forward_rate): #current forward rate, should be called if new forward rate data is available
-        self.current_forward_rate = current_forward_rate
-        return self.current_forward_rate
+    def update_cddr(self, imu_fwd_rate): #current fwd rate, should be called if new fwd rate data is available
+        self.current_detectable_fwd_rate = imu_fwd_rate
+        return self.current_detectable_fwd_rate
     
-    def update_dtz(self): #desired thrust z, should be called after desired forward rate is calculated
-        self.desired_thrust_z = self.forward_rate_controller.update(self.desired_forward_rate, self.current_forward_rate, self.dt)
-        return self.desired_thrust_z
-        
-    #def update_all(self, target_state, current_state): #should not be used in the end
-        # Get current and target forwards
-        self.update_cd(current_state)
-        self.update_dd(target_state)
-        # Update forward rate
-        self.update_ddr()
-        self.update_cdr(current_state)
-        # Update thrust_z
-        thrust_z = self.update_dtz()
+    def update_dt(self): #desired thrust, should be called after desired fwd rate is calculated
+        self.desired_thrust = self.fwd_rate_controller.update(self.desired_fwd_rate, self.current_detectable_fwd_rate, self.dt)
+        self.current_detectable_thrust = self.desired_thrust
+        print("desired thrust: ", self.desired_thrust)
+        return self.desired_thrust
 
-        return thrust_z
+    #def update_fwd_and_rate(fwd, rate):
+        self.current_fwd = fwd
+        self.current_fwd_rate = rate
+
+    def append_to_state(self, state):
+        state['detectable fwd'] = self.current_detectable_fwd
+        state['detectable rate'] = self.current_detectable_fwd_rate
+        state['detectable thrust'] = self.current_detectable_thrust
