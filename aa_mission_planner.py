b0ff466333e3e986ba49bd8cdd6bbbf415ea329f
alexfactory3 <asheridan@ethz.ch>
2025-03-05 15:22:55 +0000

    description

--- aa_mission_planner.py
+++ aa_mission_planner.py
@@ -4,10 +4,9 @@ from Communication.transceive_functions import *
 if __name__ == "__main__":
     lora_transceiver = LoRa_Transceiver(device_id=1, slot_duration=1.0)
 
-    message = 
     lora_transceiver.send_message([111.1, 21.2, 71.1])
     lora_transceiver.receive_LoRa()
-    #test
+
 
 
 
