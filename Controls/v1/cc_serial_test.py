5c475bf8aa447670c83d6436c91bffda8fd9f0b2
Nino Liner <nliner@ethz.ch>
2025-03-05 09:12:39 +0000

    moving code from test platform to github

--- Controls/v1/cc_serial_test.py
+++ Controls/v1/cc_serial_test.py
@@ -1,39 +1,72 @@
 import serial
 import numpy as np
 import keyboard
+import sys
+import threading
+
+
+def send_input():
+    while True:
+        key = sys.stdin.read(1)  # Read one character at a time
+        if key == '1':
+            ser.write(b'\n1')
+            print("Sent: 1")
+        elif key == '0':
+            ser.write(b'\n0')
+            print("Sent: 0")
+
+
 
 if __name__ == "__main__":
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
     ser.flush()
 
     orientation = np.array([])
     quat_offsets = np.array([])
     quat = np.array([])
     calibration = np.array([])
+    i=0
+    j=0
+    k=0
+    l=0
+    m=0
+
+    print("hi")
+
+
+    #Start input thread
+    threading.Thread(target=send_input, daemon=True).start()
 
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8', errors='replace').strip()
             if "Orientation" in line:
+                i+=1
                 values = line.split(":")[-1].strip()
                 orientation = np.array([float(x) for x in values.split(",")])
-                print("Orientation:", orientation)
+                if (i%50 == 0): print("Orientation:", orientation) 
             elif "Quaternion rel. to offsets" in line:
+                j+=1
                 values = line.split(":")[-1].strip()
                 quat_offsets = np.array([float(x) for x in values.split(",")])
-                print("Quaternion relative to offsets:", quat_offsets)
+                if (j%50 == 0): print("Quaternion relative to offsets:", quat_offsets)
             elif "Quaternion:" in line and "rel. to offsets" not in line:
+                k+=1
                 values = line.split(":")[-1].strip()
                 quat = np.array([float(x) for x in values.split(",")])
-                print("Quaternion:", quat)
+                if (k%50 == 0): print("Quaternion:", quat)
             elif "Calibration" in line:
+                l+=1
                 values = line.split(":")[-1].strip()
                 calibration = np.array([int(x) for x in values.split(",")])
-                print("Calibration:", calibration)
+                if (l%50 == 0): print("Calibration:", calibration)
             elif "Angular Rates (x,y,z):" in line:
+                m+=1
                 values = line.split(":")[-1].strip()
-                angular_rates = np.array([int(x) for x in values.split(",")])
-                print("Angular Rates (x,y,z):", angular_rates)
+                #print(values)
+                #print([str(x) for x in values.split(",")])
+                angular_rates = np.array([float(x) for x in values.split(",") if x.strip() != ""])
+                if (m%50 == 0): print("Angular Rates (x,y,z):", angular_rates)
             else:
                 print("Unknown line:", line)
                 #print(line)
@@ -47,6 +80,7 @@ if __name__ == "__main__":
             ser.write(b'0')   # Send '1'
             print("Sent: 0")
 
+        
 
 
 
--- Controls/v1/dd_orient_ctrl_test.py
+++ Controls/v1/dd_orient_ctrl_test.py
@@ -12,6 +12,56 @@ import numpy as np
 import keyboard
 import time
 
+import sys
+import threading
+
+
+running = True
+
+
+def send_input():
+    while True:
+        key = sys.stdin.read(1)  # Read one character at a time
+        if key == '1':
+            ser.write(b'\n1')
+            print("Sent: 1")
+        elif key == '0':
+            ser.write(b'\n0')
+            print("Sent: 0")
+        elif key == '9':
+            running = not running
+            print("Motors off: 9")
+
+def receive_input():
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8', errors='replace').strip()
+            if "Orientation" in line:
+                values = line.split(":")[-1].strip()
+                orientation = np.array([float(x) for x in values.split(",")])
+                #print("Orientation:", orientation)
+            elif "Quaternion rel. to offsets" in line:
+                values = line.split(":")[-1].strip()
+                quat_w_offsets = np.array([float(x) for x in values.split(",")])
+                #print("Quaternion relative to offsets:", quat_w_offsets)
+            elif "Quaternion:" in line and "rel. to offsets" not in line:
+                values = line.split(":")[-1].strip()
+                quat = np.array([float(x) for x in values.split(",")])
+                #print("Quaternion:", quat)
+            elif "Angular Rates (x,y,z):" in line:
+                values = line.split(":")[-1].strip()
+                #print(values)
+                #print([str(x) for x in values.split(",")])
+                angular_rates = np.array([float(x) for x in values.split(",") if x.strip() != ""])
+                #print("Angular Rates (x,y,z):", angular_rates)
+            elif "Calibration" in line:
+                values = line.split(":")[-1].strip()
+                calibration = np.array([int(x) for x in values.split(",")[-1].strip()])
+                #print("Calibration:", calibration) 
+            else:
+                pass
+
+
 
 if __name__ == "__main__":
 
@@ -20,75 +70,123 @@ if __name__ == "__main__":
 
     #initialize llc with initial state and store time 
     llc = LLC2(CONSTANTS['pid_params'], CONSTANTS['init_params'])
+    sca = SCA(llc)
     last_update = time.time()
 
     #3x2 array for roll, pitch, yaw and their rates from the init_params
-    angle_rates = np.zeros(3)
+    angular_rates = np.zeros(3)
+
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+    ser.flush()
 
     with open('total_state.log', 'ab') as log:
 
-        for i in range(1,1000):
+        while running:
+
             #planner updates (1000 should be 10s due to 100Hz controller & IMU freq.)
 
             #set up serial connection
-            ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-            ser.flush()
 
             #initialize variables
-            orientation = np.array([])
-            quat_w_offsets = np.array([])
+            orientation = np.zeros(3)
+            quat_w_offsets = np.zeros(4)
             quat = np.array([])
             calibration = np.array([])
 
-            if ser.in_waiting > 0:
-                line = ser.readline().decode('utf-8', errors='replace').strip()
-                if "Orientation" in line:
-                    values = line.split(":")[-1].strip()
-                    orientation = np.array([float(x) for x in values.split(",")])
-                    print("Orientation:", orientation)
-                elif "Quaternion rel. to offsets" in line:
-                    values = line.split(":")[-1].strip()
-                    quat_w_offsets = np.array([float(x) for x in values.split(",")])
-                    print("Quaternion relative to offsets:", quat_w_offsets)
-                elif "Quaternion:" in line and "rel. to offsets" not in line:
-                    values = line.split(":")[-1].strip()
-                    quat = np.array([float(x) for x in values.split(",")])
-                    print("Quaternion:", quat)
-                elif "Angular rates" in line:
-                    values = line.split(":")[-1].strip()
-                    angle_rates = np.array([float(x) for x in values.split(",")])
-                    print("Angular rates:", angle_rates)
-                elif "Calibration" in line:
-                    values = line.split(":")[-1].strip()
-                    calibration = np.array([int(x) for x in values.split(",")])
-                    print("Calibration:", calibration) 
-                else:
-                    print(line)
 
-                if angle_rates.size == 3 and quat.size == 4:
-                    llc.update_actual_local_rates(angle_rates)
+            #Start input thread
+            threading.Thread(target=send_input, daemon=True).start()
+
+            print("type 1 now")
+            sca.update_motor_thrusts_manual(0, 0, 0)
+
+
+            print("starting up")
+            for i in range(1,10000):
+
+                if i>1:
+
+                    llc.global_orientation_estimate_quat = np.array([1,1,0,0])
+
+                for k in range(1):
+
+
+                    for j in range(6):
+                    
+                        if ser.in_waiting > 0:
+                            line = ser.readline().decode('utf-8', errors='replace').strip()
+                            if "Orientation" in line:
+                                values = line.split(":")[-1].strip()
+                                orientation = np.array([float(x) for x in values.split(",")])
+                                #print("Orientation:", orientation)
+                            elif "Quaternion rel. to offsets" in line:
+                                values = line.split(":")[-1].strip()
+                                quat_w_offsets = np.array([float(x) for x in values.split(",")])
+                                #print("Quaternion relative to offsets:", quat_w_offsets)
+                            elif "Quaternion:" in line and "rel. to offsets" not in line:
+                                values = line.split(":")[-1].strip()
+                                quat = np.array([float(x) for x in values.split(",")])
+                                #print("Quaternion:", quat)
+                            elif "Angular Rates (x,y,z):" in line:
+                                values = line.split(":")[-1].strip()
+                                #print(values)
+                                #print([str(x) for x in values.split(",")])
+                                angular_rates = np.array([float(x) for x in values.split(",") if x.strip() != ""])
+                                #print("Angular Rates (x,y,z):", angular_rates)
+                            elif "Calibration" in line:
+                                values = line.split(":")[-1].strip()
+                                calibration = np.array([int(x) for x in values.split(",")[-1].strip()])
+                                #print("Calibration:", calibration) 
+                            else:
+                                pass
+                                #print(line)
+
+                    
+
+                    #if True:#(angular_rates.size == 3 and quat_w_offsets.size == 4 and orientation.size == 3):
+                    
+                    llc.update_actual_local_rates(angular_rates[0],angular_rates[1],angular_rates[2])
                     #pass controller the new angular rates
 
-                    llc.global_orientation_estimate_quat = np.array(quat_w_offsets[1],quat_w_offsets[2],quat_w_offsets[3],quat_w_offsets[0])
+                    llc.update_global_orientation_w_quat_sf(quat_w_offsets)
                     #TODO: update the LLC with a function that reads scalar first quat and stores them scalar last and normalize the quaternion
                     #done
 
                     torquex, torquey, torquez = llc.update_w_mode1(time.time() - last_update)
                     #calculate the torques
-                    #TODO: change update_w_mode1 to take real dt's (time.time() - last_update)
+                    #TODo: change update_w_mode1 to take real dt's (time.time() - last_update)
                     #done
                     
+
+                        #wait for 0.01 - (time.time() - last_update) seconds
+                        #time.sleep(0.01 - (time.time() - last_update))
+
+                    if(i%100 == 0):
+                        print(f"Quaternion {quat}")
+                        print(f"Euler {orientation}")
+                        print(f"Torques {torquex} {torquey} {torquez}")#print(torquex,torquey,torquez, quat_w_offsets, orientation) #zyx euler angles
+                        print(f"Motor signals {sca.motor_signals}")
+                        print(f"Motor signals {time.time()} - {last_update}")
+
                     last_update = time.time()
 
-                    #wait for 0.01 - (time.time() - last_update) seconds
-                    time.sleep(0.01 - (time.time() - last_update))
+                    
+                    sca.update_motor_thrusts_manual(torquex, torquey, torquez)
 
-                    SCA.update_motor_thrusts_manual(torquex,torquey,torquez)
-                    #update the motor thrusts
+                    #log rate rates in a log file
+                    np.savetxt(log, np.array([[orientation[0], sca.motor_signals[0]]]), delimiter=',')
 
 
-            #log rate rates in a log file
-            np.savetxt(log, angle_rates, delimiter=',')
+
+
+                sca.update_motor_thrusts_manual(0, 0, 0)
+                time.sleep(0.5)
+            #update the motor thrusts
+
+
+
+            """
+
 
 
             #listen for offsets turned on/off
@@ -100,3 +198,5 @@ if __name__ == "__main__":
                 ser.write(b'\n')  # Send newline
                 ser.write(b'0')   # Send '1'
                 print("Sent: 0")
+
+            """
